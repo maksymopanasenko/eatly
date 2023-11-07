55e21ae85f7c5d9769105691079e391c4cc54088
maksymopanasenko <maxpanas008@gmail.com>
2023-11-07 23:55:28 +0100

    create footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import { Route, Routes } from "react-router-dom";
-import Layout from "./pages/Layout/Layout";
-import Menu from "./pages/Menu/Menu";
-import Blog from "./pages/Blog/Blog";
-import Pricing from "./pages/Pricing/Pricing";
-import Contact from "./pages/Contact/Contact";
-import NoPage from "./pages/NoPage/NoPage";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Menu />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NoPage />} />
-      </Route>
-    </Routes>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
